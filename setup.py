81e0886ee7c3a1be743de269fd8d2b20d79e484c
mast3rbow <bryton@curedcompliance.com>
2023-09-22 20:09:01 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open('README.md', 'r') as f:
 
 setuptools.setup(
     name='django_tenants_q',
-    version='1.1.0',
-    author='Chaitanya Devale',
-    author_email='chaitanyadevale11@gmail.com',
+    version='1.2.0',
+    author='Chaitanya Devale, Bryton Wishart',
+    author_email='mast3rbow@gmail.com',
     include_package_data=True,
-    description='An integration package for Django-Q with Django Tenants',
+    description='An integration package for Django-Q2 with Django Tenants',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    url='https://github.com/chaitanyadevle/django-tenants-q',
+    url='https://github.com/mast3rbow/django_tenants_q',
     license='MIT',
     packages=setuptools.find_packages(exclude=["test_project", "test-compose.yml"]),
     classifiers=[
