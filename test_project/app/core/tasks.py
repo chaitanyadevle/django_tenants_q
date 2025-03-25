fa0233d9f87321bd40c1f3e8a0d979e11a5286fb
ChaitanyaDevale <chaitanyadevale11@gmail.com>
2025-03-25 18:13:52 +0530

    Deleted unwanted code

--- test_project/app/core/tasks.py
+++
@@ -1,9 +0,0 @@
-from django.contrib.auth.models import User
-
-
-def print_users_in_tenant():
-    print([x.username for x in User.objects.all()])
-
-
-def print_emails_of_users_in_tenant():
-    print([x.email for x in User.objects.all()])
